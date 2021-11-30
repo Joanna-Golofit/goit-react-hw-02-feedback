8a9fb1d9afe823f2c9ef85bccf38e3bea5cec49f
Joanna-Golofit <sunderlajsa@gmail.com>
2021-11-30 15:12:44 +0100

    krok 1: poprawiona logika

--- src/components/statistics/Statistics.jsx
+++ src/components/statistics/Statistics.jsx
@@ -6,37 +6,18 @@ import Button from "../button/Button";
 
 class Statistics extends Component {
   state = {
-    good: 1,
-    neutral: 2,
+    good: 0,
+    neutral: 0,
     bad: 0,
   };
 
   handleClick = (evt) => {
-    // this.setState((state) => ({
-    //   ...state,
-    //   count: state.count + this.props.step,
-    // }));
-    console.log(`Button ${evt.target.innerHTML} was clicked!`); // działa
-    // console.log(evt.target.innerHTML === "Good");
-    // console.log(evt.target.innerHTML === "Neutral");
-    if (evt.target.innerHTML === "Good") {
-      this.setState((state) => ({
-        ...state,
-        good: state.good + 1,
-      }));
-      console.log(this.state);
-    } else if (evt.target.innerHTML === "Neutral") {
-      this.setState((state) => ({
-        ...state,
-        neutral: state.neutral + 1,
-      }));
-    } else {
-      this.setState((state) => ({
-        ...state,
-        bad: state.bad + 1,
-      }));
-    }
-    // console.log("this.props: ", this.props);
+    
+     this.setState((state) => ({
+      //  ...state,
+       [evt.target.innerHTML]: state[evt.target.innerHTML] + 1,
+     }));
+    
   };
 
   render() {
@@ -44,9 +25,9 @@ class Statistics extends Component {
     return (
       <div className={style.statistics}>
         <h1>Please leave feedback</h1>
-        <Button label="Good" onClick={this.handleClick} />
-        <Button label="Neutral" onClick={this.handleClick} />
-        <Button label="Bad" onClick={this.handleClick} />
+        <Button label="good" onClick={this.handleClick} />
+        <Button label="neutral" onClick={this.handleClick} />
+        <Button label="bad" onClick={this.handleClick} />
 
         <h1>Statistics</h1>
 
@@ -61,3 +42,71 @@ class Statistics extends Component {
 }
 
 export default Statistics;
+
+
+
+
+//==============z zapiskami
+// class Statistics extends Component {
+//   state = {
+//     good: 0,
+//     neutral: 0,
+//     bad: 0,
+//   };
+
+//   handleClick = (evt) => {
+//     // this.setState((state) => ({
+//     //   ...state,
+//     //   count: state.count + this.props.step,
+//     // }));
+//     // console.log(`Button ${evt.target.innerHTML} was clicked!`); // działa
+//     // console.log(evt.target.innerHTML === "Good");
+//     // console.log(evt.target.innerHTML === "Neutral");
+//      this.setState((state) => ({
+//        ...state,
+//        [evt.target.innerHTML]: state[evt.target.innerHTML] + 1,
+//      }));
+//     //  console.log(this.state);
+
+//     // if (evt.target.innerHTML === "good") {
+//     //   this.setState((state) => ({
+//     //     ...state,
+//     //     good: state.good + 1,
+//     //   }));
+//     //   console.log(this.state);
+//     // } else if (evt.target.innerHTML === "Neutral") {
+//     //   this.setState((state) => ({
+//     //     ...state,
+//     //     neutral: state.neutral + 1,
+//     //   }));
+//     // } else {
+//     //   this.setState((state) => ({
+//     //     ...state,
+//     //     bad: state.bad + 1,
+//     //   }));
+//     // }
+//     // console.log("this.props: ", this.props);
+//   };
+
+//   render() {
+//     const { good, neutral, bad } = this.state;
+//     return (
+//       <div className={style.statistics}>
+//         <h1>Please leave feedback</h1>
+//         <Button label="good" onClick={this.handleClick} />
+//         <Button label="neutral" onClick={this.handleClick} />
+//         <Button label="bad" onClick={this.handleClick} />
+
+//         <h1>Statistics</h1>
+
+//         <ul className={style.list}>
+//           <li>Good: {good}</li>
+//           <li>Neutral: {neutral}</li>
+//           <li>Bad: {bad}</li>
+//         </ul>
+//       </div>
+//     );
+//   }
+// }
+
+// export default Statistics;
